4b7d3c50454dd9c62ed5c10cb79e03ed3d0493fe
Irissf <iris.seijo@gmail.com>
2021-12-27 11:55:42 +0100

    Agregar archivos de proyecto.

---
+++ AprendiendoVB/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    'Módulo de nuestro proyecto
+
+    Sub Main()
+
+        'Variables y constantes
+        Dim variable As Integer = 12
+        Dim CONSTANTE As Integer = 1
+
+        Console.WriteLine("Imprimir mensaje es como en c#")
+        Console.WriteLine("El numero:" & variable)
+
+
+        'Pongo readline para que no se me cierre la consola, espera a que pulsemos una tecla
+        Console.ReadKey(True)
+
+    End Sub
+
+End Module
