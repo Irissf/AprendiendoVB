568f4e7f4c8172f2093a80ed655201e251a0904b
Irissf <iris.seijo@gmail.com>
2022-01-10 10:16:58 +0100

    Herencias

---
+++ AprendiendoVB/HijoClase.vb
@@ -0,0 +1,13 @@
+﻿Public Class HijoClase
+    Inherits POO
+    'inherits, es para indicar la herencia
+
+    Public puertas As Integer
+
+    Public Sub New(modelo As String, marca As String, puertas As Integer)
+        'el super seria
+        MyBase.New(marca, puertas) 'se lo mandamos al padre
+        Me.puertas = puertas
+    End Sub
+
+End Class
--- AprendiendoVB/POO.vb
+++ AprendiendoVB/POO.vb
@@ -39,4 +39,5 @@
         Return "Hola " & nombre & " el color del " & Me.marca & " " & Me.modelo & " es: " & color
     End Function
 
+
 End Class
